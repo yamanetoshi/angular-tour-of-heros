b452faa4274cf219824007a6e5c6bd77aa8bc01b
YAMANE Toshiaki <yamanetoshi@gmail.com>
2018-03-27 00:27:09 +0000

    add herosComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
