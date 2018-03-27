1b7719cf872526f664c1e8f687c26133fed78cb3
YAMANE Toshiaki <yamanetoshi@gmail.com>
2018-03-27 01:54:50 +0000

    add detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HerosComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -11,7 +11,7 @@ export class HerosComponent implements OnInit {
   
   heroes = HEROES;
   
-  selectedHero: Hero:
+  selectedHero: Hero;
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
